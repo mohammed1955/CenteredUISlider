168a35036ea674840306b963770e15c821d339b8
mohammed reza <mohammed.r.ghate@gmail.com>
2021-03-08 16:03:25 +0330

    package updated

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "CenterdUISlider",
+    name: "CenteredUISlider",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "CenterdUISlider",
-            targets: ["CenterdUISlider"]),
+            name: "CenteredUISlider",
+            targets: ["CenteredUISlider"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "CenterdUISlider",
+            name: "CenteredUISlider",
             dependencies: []),
         .testTarget(
-            name: "CenterdUISliderTests",
-            dependencies: ["CenterdUISlider"]),
+            name: "CenteredUISliderTests",
+            dependencies: ["CenteredUISlider"]),
     ]
 )
--- Tests/CenteredUISliderTests/CenteredUISliderTests.swift
+++ Tests/CenteredUISliderTests/CenteredUISliderTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import CenterdUISlider
 
-final class CenterdUISliderTests: XCTestCase {
+final class CenteredUISliderTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
